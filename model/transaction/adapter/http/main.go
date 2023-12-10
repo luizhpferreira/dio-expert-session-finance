ba044b704472ce90d3d64659c387c564e9492f6d
Luiz <luizferreiralps@gmail.com>
2023-12-10 19:27:04 -0300

    criando metrics

--- model/transaction/adapter/http/main.go
+++ model/transaction/adapter/http/main.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/luizhpferreira/dio-expert-session-finance/model/transaction/adapter/http/actuator"
 	"github.com/luizhpferreira/dio-expert-session-finance/model/transaction/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.HandleFunc("/health", actuator.Health)
 
